54f564cd0396d84718e7c51a78e414d4b8b76cfc
adampauls <adpauls@microsoft.com>
2021-05-18 13:47:19 -0700

    Normalize numbers in try_round_trip (#29)

--- src/dataflow/core/lispress.py
+++ src/dataflow/core/lispress.py
@@ -49,7 +49,8 @@ Lispress = Sexp
 def try_round_trip(lispress_str: str) -> str:
     """
     If `lispress_str` is valid lispress, round-trips it to and from `Program`.
-    This puts named arguments in alphabetical order.
+    This puts named arguments in alphabetical order and normalizes numbers
+    so they all have exactly one decimal place.
     If it is not valid, returns the original string unmodified.
     """
     try:
@@ -57,7 +58,18 @@ def try_round_trip(lispress_str: str) -> str:
         lispress = parse_lispress(lispress_str)
         program, _ = lispress_to_program(lispress, 0)
         round_tripped = program_to_lispress(program)
-        return render_compact(round_tripped)
+
+        def normalize_numbers(exp: Lispress) -> "Lispress":
+            if isinstance(exp, str):
+                try:
+                    num = float(exp)
+                    return f"{num:.1f}"
+                except ValueError:
+                    return exp
+            else:
+                return [normalize_numbers(e) for e in exp]
+
+        return render_compact(normalize_numbers(round_tripped))
     except Exception:  # pylint: disable=W0703
         return lispress_str
 
