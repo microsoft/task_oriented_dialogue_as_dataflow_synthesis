99c1b7a0f016f17c59a34dda3ab607b5db66935c
Subhro Roy <subhrroy@microsoft.com>
2021-05-31 01:34:39 -0700

    format

--- src/dataflow/core/lispress.py
+++ src/dataflow/core/lispress.py
@@ -71,8 +71,8 @@ def try_round_trip(lispress_str: str) -> str:
 
         def strip_copy_strings(exp: Lispress) -> "Lispress":
             if isinstance(exp, str):
-                if len(exp) > 2 and exp[0] == "\"" and exp[-1] == "\"":
-                    return "\"" + exp[1:-1].strip() + "\""
+                if len(exp) > 2 and exp[0] == '"' and exp[-1] == '"':
+                    return '"' + exp[1:-1].strip() + '"'
                 else:
                     return exp
             else:
