383e4a98dec945fae4b7c7e1eb6f57864188dd51
adampauls <adpauls@microsoft.com>
2021-06-21 11:07:48 -0700

    Fix parsing of tokenized strings with meta characters (#43)

--- src/dataflow/core/linearize.py
+++ src/dataflow/core/linearize.py
@@ -3,7 +3,6 @@
 """
 Tools for linearizing a program so that it's easier to predict with a seq2seq model.
 """
-import re
 from typing import List, Tuple
 
 from dataflow.core.lispress import (
@@ -14,10 +13,11 @@ from dataflow.core.lispress import (
     lispress_to_program,
     program_to_lispress,
     render_compact,
+    strip_copy_strings,
 )
 from dataflow.core.program import Program
 from dataflow.core.program_utils import Idx, OpType
-from dataflow.core.sexp import Sexp
+from dataflow.core.sexp import Sexp, parse_sexp
 
 
 def to_canonical_form(tokenized_lispress: str) -> str:
@@ -42,87 +42,23 @@ def program_to_seq(program: Program) -> List[str]:
 
 
 def lispress_to_seq(lispress: Lispress) -> List[str]:
-    seq = sexp_to_seq(lispress)
-    return sexp_formatter(seq)
+    return sexp_to_seq(lispress)
 
 
 def seq_to_lispress(seq: List[str]) -> Lispress:
-    deformatted = sexp_deformatter(seq)
-    return seq_to_sexp(deformatted)
-
-
-def sexp_deformatter(sexp_tokens: List[str]) -> List[str]:
-    """Converts a printed s-expression back to the original s-expression.
-
-    The inverse of sexp_formatter.
-    """
-    sexp_str = " ".join(sexp_tokens)
-
-    sexp_str = re.sub(
-        r'{ "schema" : "(\S+)" , "underlying" : " ([\S+\s*]+?) " }',
-        r'{ "schema" : "\1" , "underlying" : "\2" }',
-        sexp_str,
-    )
-
-    sexp_str = re.sub(
-        rf'{OpType.Value.value} \( (\S+) " ([\S+\s*]+?) " \)',
-        rf'{OpType.Value.value} ( \1 "\2" )',
-        sexp_str,
-    )
-    return sexp_str.split()
-
-
-def sexp_formatter(sexp_tokens: List[str]) -> List[str]:
-    """The printer for an s-expression.
-
-    Inserts spaces after/before the leading/ending double quotes for value s-expressions within the s-expression.
-    """
-    sexp_str = " ".join(sexp_tokens)
-    # Insert spaces around the quotes in a value (use non-greedy match here)
-    sexp_str = re.sub(
-        rf'{OpType.Value.value} \( (\S+) "([\S+\s*]+?)" \)',
-        rf'{OpType.Value.value} ( \1 " \2 " )',
-        sexp_str,
-    )
-
-    # complex struct in ValueOp.value.underlying
-    sexp_str = re.sub(
-        r'{ "schema" : "(\S+)" , "underlying" : "([\S+\s*]+?)" }',
-        r'{ "schema" : "\1" , "underlying" : " \2 " }',
-        sexp_str,
-    )
-
-    return sexp_str.split()
+    return strip_copy_strings(parse_sexp(" ".join(seq)))
 
 
 def sexp_to_seq(s: Sexp) -> List[str]:
     if isinstance(s, list):
         if len(s) == 3 and s[0] == META_CHAR:
-            return [META_CHAR] + [y for x in [s[1], s[2]] for y in sexp_to_seq(x)]
+            (_meta, type_meta, expr) = s
+            return [META_CHAR] + [y for x in [type_meta, expr] for y in sexp_to_seq(x)]
+        elif len(s) == 2 and s[0] == OpType.Value.value:
+            (_value, expr) = s
+            return [OpType.Value.value] + [y for x in [expr] for y in sexp_to_seq(x)]
         return [LEFT_PAREN] + [y for x in s for y in sexp_to_seq(x)] + [RIGHT_PAREN]
+    elif isinstance(s, str) and len(s) >= 2 and s[0] == '"' and s[-1] == '"':
+        return ['"'] + s[1:-1].strip().split(" ") + ['"']
     else:
         return [s]
-
-
-def seq_to_sexp(seq: List[str], sloppy: bool = False) -> Sexp:
-    stack: List[List[Sexp]] = []
-    for i, s in enumerate(seq):
-        if s == LEFT_PAREN:
-            stack.append([])
-        elif s == RIGHT_PAREN:
-            if stack:
-                closed = stack.pop()
-            elif sloppy:
-                break
-            else:
-                raise Exception(f"too many close parens (token {i} in {seq})")
-            if stack:
-                stack[-1].append(closed)
-            else:
-                return closed
-        else:
-            if stack:
-                stack[-1].append(s)
-            else:
-                return s
-    return stack
--- tests/test_dataflow/core/test_linearize.py
+++ tests/test_dataflow/core/test_linearize.py
@@ -1,70 +1,15 @@
 #  Copyright (c) Microsoft Corporation.
 #  Licensed under the MIT license.
 import json
-from typing import List
 
 from dataflow.core.linearize import (
     lispress_to_seq,
-    program_to_seq,
-    seq_to_program,
-    seq_to_sexp,
-    sexp_deformatter,
-    sexp_formatter,
+    seq_to_lispress,
     sexp_to_seq,
+    to_canonical_form,
 )
-from dataflow.core.lispress import parse_lispress, unnest_line
-from dataflow.core.program import Expression, Program, ValueOp
-from dataflow.core.sexp import Sexp
-
-
-def test_plan_to_seq_strict():
-    """Strict tests the conversion between the Program and the linearized plan."""
-    original_program = Program(
-        expressions=[
-            Expression(
-                id="[1]",
-                op=ValueOp(
-                    value=json.dumps(
-                        {
-                            "schema": "Placeholder[List[CreateCommitEvent]]",
-                            "underlying": {
-                                "error": {
-                                    "schema": "EmptySlot",
-                                    "underlying": {
-                                        "slotId": {
-                                            "schema": "String",
-                                            "underlying": "255905761",
-                                        }
-                                    },
-                                }
-                            },
-                        }
-                    )
-                ),
-            ),
-        ]
-    )
-    expected_linearized_plan = (
-        "( # ( Placeholder[List[CreateCommitEvent]] "
-        '{ "error" : { "schema" : "EmptySlot" , '
-        '"underlying" : { "slotId" : { "schema" : "String" , "underlying" : " 255905761 " } } '
-        "} } "
-        ") )"
-    )
-
-    def flatten(form: Sexp) -> List[str]:
-        return (
-            [form]
-            if not isinstance(form, list)
-            else [s for subexp in form for s in flatten(subexp)]
-        )
-
-    linearized_plan_tokens = program_to_seq(program=original_program)
-    assert all(" " not in tok for tok in flatten(linearized_plan_tokens))
-    assert " ".join(linearized_plan_tokens) == expected_linearized_plan
-
-    program, _ = seq_to_program(seq=linearized_plan_tokens, idx=0)
-    assert program == original_program
+from dataflow.core.lispress import parse_lispress, render_compact, unnest_line
+from dataflow.core.program import Expression, ValueOp
 
 
 def test_sexp_to_seq_is_invertible():
@@ -76,7 +21,7 @@ def test_sexp_to_seq_is_invertible():
     ]
     for orig in sexps:
         seq = sexp_to_seq(orig)
-        result = seq_to_sexp(seq)
+        result = seq_to_lispress(seq)
         assert result == orig
 
 
@@ -94,34 +39,25 @@ def test_unnest_line():
     assert expressions[0] == expected_expression
 
 
-def test_sexp_formatter_and_deformatter():
+def test_linearized_roundtrip():
     """Round-trip tests for s-expression formatter and deformatter."""
     data = [
-        ("", ""),
-        ('# ( String "singleToken" )', '# ( String " singleToken " )'),
-        ('# ( String "multiple tokens" )', '# (  String " multiple tokens " )'),
-        # these two are not valid value s-expressions, so they are not formatted
-        ('# ( String "open quote only )', '# (  String "open quote only )'),
-        ('# ( String closing quote only" )', '# ( String closing quote only" )',),
-        # multiple value s-expressions
-        (
-            '# ( String "singleToken" ) # ( String "multiple tokens" )',
-            '# ( String " singleToken " ) # ( String " multiple tokens " )',
-        ),
+        ('#(String "singleToken")', '# ( String " singleToken " )'),
+        ('#(String "multiple tokens")', '# (  String " multiple tokens " )'),
         # real data
         (
-            '( ( mapGet ( # ( Path "fare.fare_id" ) ) ( clobberRevise ( getSalient ( actionIntensionConstraint ) ) ( '
-            "Constraint[Constraint[flight]] ) ( Constraint "
-            ':type ( ?= ( # ( String "fare" ) ) ) ) ) ) )',
-            '( ( mapGet ( # ( Path " fare.fare_id " ) ) ( clobberRevise ( getSalient ( actionIntensionConstraint ) ) '
+            '((mapGet #(Path "fare.fare_id") (clobberRevise (getSalient (actionIntensionConstraint)) ('
+            "Constraint[Constraint[flight]]) (Constraint "
+            ':type (?= #(String "fare"))))))',
+            '( ( mapGet # ( Path " fare.fare_id " ) ( clobberRevise ( getSalient ( actionIntensionConstraint ) ) '
             "( Constraint[Constraint[flight]] ) ( "
-            'Constraint :type ( ?= ( # ( String " fare " ) ) ) ) ) ) )',
+            'Constraint :type ( ?= # ( String " fare " ) ) ) ) ) )',
         ),
     ]
 
     for raw_sexp, formatted_sexp in data:
-        assert sexp_formatter(raw_sexp.split()) == formatted_sexp.split()
-        assert sexp_deformatter(formatted_sexp.split()) == raw_sexp.split()
+        assert lispress_to_seq(parse_lispress(raw_sexp)) == formatted_sexp.split()
+        assert render_compact(seq_to_lispress(formatted_sexp.split())) == raw_sexp
 
 
 def test_meta():
@@ -139,3 +75,16 @@ def test_meta():
         ")",
         ")",
     ]
+
+
+def test_meta_to_canonical():
+    s = """( Yield ( Execute ( ReviseConstraint ( refer ( ^ ( Dynamic ) roleConstraint ( Path.apply "output" ) ) ) ( ^ ( Event ) ConstraintTypeIntension ) ( Event.showAs_? ( ?= ( ShowAsStatus.OutOfOffice ) ) ) ) ) )"""
+    assert (
+        to_canonical_form(s)
+        == """(Yield (Execute (ReviseConstraint (refer (^(Dynamic) roleConstraint (Path.apply "output"))) (^(Event) ConstraintTypeIntension) (Event.showAs_? (?= (ShowAsStatus.OutOfOffice))))))"""
+    )
+
+
+def test_quoted():
+    s = """( foo " bar " )"""
+    assert to_canonical_form(s) == """(foo "bar")"""
